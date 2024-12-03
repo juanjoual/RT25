ca9282738465a7ec950a0d71c47fbc58d58d9941
Nelson G. Roman <ngroman1992@gmail.com>
2024-12-03 09:27:08 -0300

    Adicionando codigo con Adam en POO

---
+++ Adam_POO/multicore/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -o errexit
+set -o xtrace
+
+now=$(date +"%Y%m%d_%H%M%S")
+
+plan=Head-and-Neck_02
+plan_folder=~/Repo/RT25/TROTS/data/$plan
+
+results_folder=results
+result_path=$results_folder/x_$plan_$now.txt
+mkdir -p $results_folder
+
+#fluence_folder=~/RT25/gradient/fluences
+fluence_folder=~/Repo/RT25/TROTS/data/$plan
+fluence_prefix=x_
+
+make
+./adam_mkl $plan_folder $result_path $fluence_folder $fluence_prefix
