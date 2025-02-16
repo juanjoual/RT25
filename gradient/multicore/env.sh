3ab654559f12dc614fbf396be575b5ca8feccb83
Juanjo Moreno <juanjomoreno@ual.es>
2025-02-17 00:29:02 +0100

    Added multicore version for NIO patients.

---
+++ gradient/multicore/env.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/opt/intel/oneapi/setvars.sh
---
+++ gradient/multicore_nio/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -o errexit
+set -o xtrace
+
+now=$(date +"%Y%m%d_%H%M%S")
+
+plan=3
+plan=$1
+plan_folder=~/RT25/NIO/$plan
+
+results_folder=results
+result_path=$results_folder/x_$plan_$now.txt
+mkdir -p $results_folder
+
+#fluence_folder=~/RT25/gradient/fluences
+fluence_folder=~/RT25/NIO/$plan
+fluence_prefix=x_
+
+make
+./gradient_mkl $plan_folder $result_path $fluence_folder $fluence_prefix
