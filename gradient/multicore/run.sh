4d10b1969853f10e06bd76ed1ab2f685aefd6646
Nelson G. Roman <ngroman1992@gmail.com>
2025-02-20 12:20:54 -0300

    Cambiando para usar

--- gradient/multicore/run.sh
+++ gradient/multicore/run.sh
@@ -6,14 +6,14 @@ set -o xtrace
 now=$(date +"%Y%m%d_%H%M%S")
 
 plan=Head-and-Neck_02
-plan_folder=~/RT25/TROTS/data/$plan
+plan_folder=~/Repo/RT25/TROTS/data/$plan
 
 results_folder=results
 result_path=$results_folder/x_$plan_$now.txt
 mkdir -p $results_folder
 
 #fluence_folder=~/RT25/gradient/fluences
-fluence_folder=~/RT25/TROTS/data/$plan
+fluence_folder=~/Repo/RT25/TROTS/data/$plan
 fluence_prefix=x_
 
 make
