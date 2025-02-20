0c88e2ecacb8c32a3af1139de5199d9ea2440f4b
Nelson G. Roman <ngroman1992@gmail.com>
2025-02-20 12:18:36 -0300

    Cambiando ruta de archivos

--- gradient/multicore_nio/run.sh
+++ gradient/multicore_nio/run.sh
@@ -6,15 +6,15 @@ set -o xtrace
 now=$(date +"%Y%m%d_%H%M%S")
 
 plan=3
-plan=$1
-plan_folder=~/RT25/NIO/$plan
+# plan=$1
+plan_folder=~/Repo/RT25/NIO/$plan
 
 results_folder=results
 result_path=$results_folder/x_$plan_$now.txt
 mkdir -p $results_folder
 
 #fluence_folder=~/RT25/gradient/fluences
-fluence_folder=~/RT25/NIO/$plan
+fluence_folder=~/Repo/RT25/NIO/$plan
 fluence_prefix=x_
 
 make
