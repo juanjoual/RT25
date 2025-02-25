e0be322e41219fa6c48e75420e7620d222f18520
Nelson G. Roman <ngroman1992@gmail.com>
2025-02-24 21:17:48 -0300

    Cambios en el makefile y run

--- NIO_POO/multicore/run.sh
+++ NIO_POO/multicore/run.sh
@@ -9,29 +9,29 @@ plan=3
 # plan=$1
 plan_folder=~/Repo/RT25/NIO/$plan
 
-# Adam
-folder=../../Results/Adam
-results_folder=$folder/$plan 
-result_path=$results_folder/x_$plan_$now.txt
-mkdir -p $results_folder
-
-#fluence_folder=~/RT25/gradient/fluences
-fluence_folder=~/Repo/RT25/TROTS/data/$plan
-fluence_prefix=x_
-
-make
-./adam_mkl $plan_folder $result_path $fluence_folder $fluence_prefix
-
-
-# # Gardient
-# folder=../../Results/SGD
+# # Adam
+# folder=../../Results/Adam
 # results_folder=$folder/$plan 
 # result_path=$results_folder/x_$plan_$now.txt
 # mkdir -p $results_folder
 
 # #fluence_folder=~/RT25/gradient/fluences
-# fluence_folder=~/Repo/RT25/NIO/$plan
+# fluence_folder=~/Repo/RT25/TROTS/data/$plan
 # fluence_prefix=x_
 
 # make
-# ./gradient_mkl $plan_folder $result_path $fluence_folder $fluence_prefix
+# ./adam_mkl $plan_folder $result_path $fluence_folder $fluence_prefix
+
+
+# Gardient
+folder=../../Results/SGD
+results_folder=$folder/$plan 
+result_path=$results_folder/x_$plan_$now.txt
+mkdir -p $results_folder
+
+#fluence_folder=~/RT25/gradient/fluences
+fluence_folder=~/Repo/RT25/NIO/$plan
+fluence_prefix=x_
+
+make
+./gradient_mkl $plan_folder $result_path $fluence_folder $fluence_prefix
