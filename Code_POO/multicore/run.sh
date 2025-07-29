6b3ebaa6d7a560b43528cc293ae2fbc352b5557f
Nelson G. Roman <ngroman1992@gmail.com>
2025-07-29 12:45:20 -0300

    Fazendo ajustes

--- Code_POO/multicore/run.sh
+++ Code_POO/multicore/run.sh
@@ -5,8 +5,8 @@ set -o xtrace
 
 now=$(date +"%Y%m%d_%H%M%S")
 
-#plan=Head-and-Neck_07
-plan=Prostate_CK_04
+plan=Head-and-Neck_07
+# plan=Prostate_CK_03
 plan_folder=~/Repo/RT25/TROTS/data/$plan
 
 # Adam
